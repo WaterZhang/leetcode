eabd17ff4aae9279c1b5474bda2d2e783c2437dd
Water Zemiao Zhang <wzhang@expedia.com>
2021-01-25 17:08:43 +0800

    delete node in linked list

---
+++ src/main/java/com/zhangzemiao/leetcode/top/linkedlist/DeleteNodeInLinkedList.java
@@ -0,0 +1,33 @@
+package com.zhangzemiao.leetcode.top.linkedlist;
+
+/**
+ * @Desc: delete node in linked list
+ * @author: wzhang
+ * @date: 2021/1/25
+ */
+public class DeleteNodeInLinkedList {
+
+    public static void main(String[] args){
+        ListNode head = new ListNode(4);
+        ListNode second = new ListNode(5);
+        ListNode three = new ListNode(1);
+        ListNode end = new ListNode(9);
+        head.next = second;
+        second.next = three;
+        three.next = end;
+
+    }
+
+    private static void deleteNode(ListNode node){
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x){
+            this.val = x;
+        }
+    }
+}
