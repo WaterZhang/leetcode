e0dc92029e7b96252cf7cfbb08523703cd289772
Water Zemiao Zhang <wzhang@expedia.com>
2021-01-26 11:33:35 +0800

    add delete node

---
+++ src/main/java/com/zhangzemiao/leetcode/top/linkedlist/ListNode.java
@@ -0,0 +1,18 @@
+package com.zhangzemiao.leetcode.top.linkedlist;
+
+/**
+ * @Desc: Node
+ * @author: wzhang
+ * @date: 2021/1/26
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int x){
+        this.val = x;
+    }
+    ListNode(){
+
+    }
+}
