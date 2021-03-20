dd105c1637e9d2410c0c77c3446f3ffb93316606
Water Zemiao Zhang <wzhang@expedia.com>
2021-03-20 19:14:10 +0800

    update serialize and deserialize binary tree

--- src/main/java/com/zhangzemiao/leetcode/binaryTree/SerializeAndDeserializeBinaryTree.java
+++ src/main/java/com/zhangzemiao/leetcode/binaryTree/SerializeAndDeserializeBinaryTree.java
@@ -20,39 +20,56 @@ import java.util.List;
 public class SerializeAndDeserializeBinaryTree {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        TreeNode root = new TreeNode(4);
+        TreeNode l11 = new TreeNode(-7);
+        TreeNode l12 = new TreeNode(-3);
+        root.left = l11;
+        root.right = l12;
+
+        TreeNode l21 = new TreeNode(-9);
+        TreeNode l22 = new TreeNode(-3);
+        l12.left = l21;
+        l12.right = l22;
+
+        TreeNode l31 = new TreeNode(9);
+        l21.left = l31;
+
+        TreeNode l32 = new TreeNode(-7);
+        TreeNode l33 = new TreeNode(-4);
+        l21.right = l32;
+        l22.left = l33;
+
+        TreeNode l41 = new TreeNode(6);
+        l31.left = l41;
+        TreeNode l42 = new TreeNode(-6);
+        l32.left = l42;
+        TreeNode l43 = new TreeNode(-6);
+        l32.right = l43;
+
         /*
-        TreeNode root = new TreeNode(1);
-        TreeNode left = new TreeNode(2);
-        TreeNode right = new TreeNode(3);
-        root.left = left;
-        root.right = right;
+        TreeNode l51 = new TreeNode(0);
+        l41.left = l51;
+        TreeNode l52 = new TreeNode(6);
+        l41.right = l52;
+        TreeNode l53 = new TreeNode(5);
+        l42.left = l53;
+        TreeNode l54 = new TreeNode(9);
+        l43.left = l54;
 
-        TreeNode four = new TreeNode(4);
-        TreeNode five = new TreeNode(5);
-        left.left = four;
-        left.right = five;
-
-        TreeNode six = new TreeNode(6);
-        TreeNode seven = new TreeNode(7);
-        right.left = six;
-        right.right = seven;
+        TreeNode l61 = new TreeNode(-1);
+        l51.right = l61;
+        TreeNode l62 = new TreeNode(-4);
+        l52.left = l62;
+        TreeNode l63 = new TreeNode(-2);
+        l54.left = l63;
          */
 
-        TreeNode root = new TreeNode(1);
-        TreeNode two = new TreeNode(2);
-        TreeNode three = new TreeNode(3);
-        root.left = two;
-        root.right = three;
-
-        TreeNode four = new TreeNode(4);
-        TreeNode five = new TreeNode(5);
-        three.left = four;
-        three.right = five;
-
-
         String serializeStr = serialize(root);
+        System.out.println(serializeStr);
         TreeNode result = deserialize(serializeStr);
         System.out.println("test");
+        //System.out.println(Math.pow(2,3));
     }
 
 
@@ -61,7 +78,7 @@ public class SerializeAndDeserializeBinaryTree {
             return null;
         }
         List<Integer> result = new ArrayList<>();
-        traverseOneLayer(result, Arrays.asList(root));
+        traverseOneLayer(result, Arrays.asList(root), 1);
         StringBuilder sb = new StringBuilder();
         for(int i=0; i < result.size()-1; i++){
             sb.append(result.get(i)+",");
@@ -83,7 +100,7 @@ public class SerializeAndDeserializeBinaryTree {
         if(result != null && !result.isEmpty()){
             root = new TreeNode(result.get(0));
         }
-        buildTree(result.subList(1, result.size()), Arrays.asList(root));
+        buildTree(result.subList(1, result.size()), Arrays.asList(root), 1);
         return root;
     }
 
@@ -92,7 +109,7 @@ public class SerializeAndDeserializeBinaryTree {
         //traversal tree
         try {
             List<Integer> result = new ArrayList<>();
-            traverseOneLayer(result, Arrays.asList(root));
+            traverseOneLayer(result, Arrays.asList(root), 1);
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             ObjectOutputStream oos = new ObjectOutputStream(baos);
             oos.writeObject(result);
@@ -115,7 +132,7 @@ public class SerializeAndDeserializeBinaryTree {
             if(result != null && !result.isEmpty()){
                 root = new TreeNode(result.get(0));
             }
-            buildTree(result.subList(1, result.size()), Arrays.asList(root));
+            buildTree(result.subList(1, result.size()), Arrays.asList(root), 1);
             return root;
         } catch (Exception  ex){
             // log
@@ -123,44 +140,52 @@ public class SerializeAndDeserializeBinaryTree {
         }
     }
 
-    public static void buildTree(List<Integer> values, List<TreeNode> nodes){
+    public static void buildTree(List<Integer> values, List<TreeNode> nodes, int n){
         if(values.isEmpty()){
             return;
         }
         List<TreeNode> nextNodes = new ArrayList<>();
+        int removeCount = 0;
         for(int i=1; i <= nodes.size(); i++){
             TreeNode one = nodes.get(i-1);
             if(values.size() >= 2){
                 Integer leftValue = values.remove(0);
-                if(leftValue > 0){
+                removeCount++;
+                if(leftValue > Integer.MIN_VALUE){
                     one.left = new TreeNode(leftValue);
                     nextNodes.add(one.left);
                 }
                 Integer rightValue = values.remove(0);
-                if(rightValue > 0){
+                removeCount++;
+                if(rightValue > Integer.MIN_VALUE){
                     one.right = new TreeNode(rightValue);
                     nextNodes.add(one.right);
                 }
             }
         }
-        if(! values.isEmpty()){
-            buildTree(values, nextNodes);
+        for(int i = removeCount; i < (int)Math.pow(2, n); i++){
+            values.remove(0);
+        }
+        if(! values.isEmpty() && !nextNodes.isEmpty()){
+            buildTree(values, nextNodes, n+1);
         }
     }
 
-    public static void traverseOneLayer(List<Integer> result,  List<TreeNode> nodes){
+    public static void traverseOneLayer(List<Integer> result,  List<TreeNode> nodes, int n){
         if(nodes.isEmpty()){
             return;
         }
         //计算下一层的所有结点
         List<TreeNode> nextNodes = new ArrayList<>();
         int count = 0;
+        boolean allNegative = true;
         for(TreeNode node : nodes){
             count++;
             result.add(node.val);
+            /*
             if(node.val == Integer.MIN_VALUE){
                 continue;
-            }
+            }  */
             // skip if no child
             if(node.left == null && node.right == null){
                 if(count != nodes.size()){
@@ -170,16 +195,25 @@ public class SerializeAndDeserializeBinaryTree {
                 continue;
             }
             if(node.left != null){
+                allNegative = false;
                 nextNodes.add(node.left);
             } else {
-                nextNodes.add(new TreeNode(0));
+                nextNodes.add(new TreeNode(Integer.MIN_VALUE));
             }
             if(node.right != null){
+                allNegative = false;
                 nextNodes.add(node.right);
             }else {
-                nextNodes.add(new TreeNode(0));
+                nextNodes.add(new TreeNode(Integer.MIN_VALUE));
             }
         }
-        traverseOneLayer(result, nextNodes);
+        if(allNegative){
+            return;
+        }
+        int nextCount = (int) Math.pow(2, n);
+        for(int i = nextNodes.size(); i < nextCount; i++){
+            nextNodes.add(new TreeNode(Integer.MIN_VALUE));
+        }
+        traverseOneLayer(result, nextNodes, n+1);
     }
 }
