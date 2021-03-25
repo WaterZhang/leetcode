0df67bf2361f449031b4001e64d2bd9ee4a1ab18
Water Zemiao Zhang <wzhang@expedia.com>
2021-03-25 17:17:57 +0800

    linked list

---
+++ src/main/java/com/zhangzemiao/leetcode/linkedlist/twopointer/ListNode.java
@@ -0,0 +1,16 @@
+package com.zhangzemiao.leetcode.linkedlist.twopointer;
+
+/**
+ * @Desc: List Node
+ * @author: wzhang
+ * @date: 2021/3/25
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int x){
+        this.val = x;
+        next = null;
+    }
+}
